d45bdc46a32da92ec3bab4f8a02a9c94e63f6e73
KonradBM <james.cheesman@gmail.com>
2025-02-06 21:33:39 +0000

    Added lambda function and parent step up

--- scraper.py
+++ scraper.py
@@ -20,3 +20,24 @@ for job_card in job_cards:
     print(title_element.text.strip())
     print(company_element.text.strip())
     print(location_element.text.strip())
+
+# Use a lambda function to return any h2 elements containing the substring of "python"
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
+
+# debug print to see if you're bringing back python jobs
+#print(len(python_jobs))
+
+# step up to the third-level parent div of the element containing "python" to return more job data
+python_job_cards = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for card in python_job_cards:
+    title_element = job_card.find("h2", class_="title")
+    company_element = job_card.find("h3", class_="company")
+    location_element = job_card.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
